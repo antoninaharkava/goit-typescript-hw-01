2a4879d20232a418129039e001bde9a7288f26ce
antonina <tonya.garkava@gmail.com>
2024-11-25 22:06:18 +0100

    save

---
+++ src/basic/7.ts
@@ -0,0 +1,11 @@
+type Page = {
+    title: string;
+    likes: number;
+    accounts: string[];
+    status: 'open' | 'close';
+    details?: {
+      createAt: Date;
+      updateAt: Date;
+    };
+  };
+  
